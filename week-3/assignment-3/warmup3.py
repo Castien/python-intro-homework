2db8bc6778232ba5c8e46428be81147c11066d5d
Castien <eileenlowers@outlook.com>
2026-07-21 23:52:52 -0400

    Airhub revisions again.

--- week-3/assignment-3/warmup3.py
+++ week-3/assignment-3/warmup3.py
@@ -10,19 +10,14 @@
 
 # Precedence: not > and > or - Represented in ().
 
-print(not True and False)
-# False - not True is False; False and False is False
+print(not True and False) # False - not True is False; False and False is False
 
-print(True or False and False)
-# True - (False and False) (precendence) is (False); True or (False) is True
+print(True or False and False) # True - (False and False) (precendence) is (False); True or (False) is True
 
-print(not (5 > 3))
-# False - 5 > 3 is True; not True is False
+print(not (5 > 3)) # False - 5 > 3 is True; not True is False
 
-print(10 == 10 and 4 != 4)
-# False - 10 == 10 is True; 4 != 4 is False; True and False is False
+print(10 == 10 and 4 != 4) # False - 10 == 10 is True; 4 != 4 is False; True and False is False
 
-print(not False or not True)
-# True - not False is True; not True is False; True or False is True
+print(not False or not True) # True - not False is True; not True is False; True or False is True
 
 
